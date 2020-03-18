6ec18c50ba587209201559d38286befcb89a1ab3
Hansin Susatya <hansinsusatya@gmail.com>
2020-03-18 14:21:14 +0700

    Remove unused codes

--- movies/routes/movie.js
+++ movies/routes/movie.js
@@ -1,9 +1,8 @@
 import { Router } from 'express'
 import MovieController from '../controllers/movie'
-import uploadImage from '../middlewares/uploadImage'
 const router = Router()
 
-router.post('/', uploadImage, MovieController.insertMovie)
+router.post('/', MovieController.insertMovie)
 router.get('/', MovieController.fetchMovies)
 router.get('/:id', MovieController.findOne)
 router.put('/:id', MovieController.updateMovie)
