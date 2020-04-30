918097256471e2e5c3c1743d5039ff2ac053af84
Hansin Susatya <hansinsusatya@gmail.com>
2020-04-30 11:57:18 +0700

    Remove unused code

--- client/App.js
+++ client/App.js
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler'
 import React from 'react'
-import { createDrawerNavigator } from '@react-navigation/drawer'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { Provider } from 'react-redux'
